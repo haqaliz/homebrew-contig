9d475e062c7d026f75520dd7a3dc19edaf566f10
Ali Haqiqi <aliz@foresightanalytics.ca>
2026-07-06 19:17:58 +0800

    contig 0.19.0 (peak-RSS OOM memory scaling)

--- Formula/contig.rb
+++ Formula/contig.rb
@@ -9,22 +9,22 @@
 class Contig < Formula
   desc "Agentic bioinformatics analyst: the Layer-2 run, self-heal, verify, reproduce engine"
   homepage "https://github.com/haqaliz/contig"
-  version "0.18.0"
+  version "0.19.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/haqaliz/contig/releases/download/v0.18.0/contig-macos-arm64"
-      sha256 "0efca34ded26ee1cd89c5a6d7ce7c2e8449be2496f86de06379ef26542bc6d6e"
+      url "https://github.com/haqaliz/contig/releases/download/v0.19.0/contig-macos-arm64"
+      sha256 "705a444a608129fec304ad25b4a178f0e1cf8b946f0ab43d0ad8fae655c8a4c4"
     end
     on_intel do
-      url "https://github.com/haqaliz/contig/releases/download/v0.18.0/contig-macos-x86_64"
-      sha256 "6f1b278a2ccc09ef341e5a6b3a13b04648c39f1cf4cdb0d94e5b57dc9a26570f"
+      url "https://github.com/haqaliz/contig/releases/download/v0.19.0/contig-macos-x86_64"
+      sha256 "6c99be1edc73a68c3da3783c9345b12ecc236ce375faa14ec2452632ef79d625"
     end
   end
 
   on_linux do
-    url "https://github.com/haqaliz/contig/releases/download/v0.18.0/contig-linux-x86_64"
-    sha256 "07868210baa6fa48dc9d761c33f3be4757c57f5bc7aa7061a3dab73fa03d519a"
+    url "https://github.com/haqaliz/contig/releases/download/v0.19.0/contig-linux-x86_64"
+    sha256 "ae35cceaeb0a381358943459295792f6c0a5c8dd8a97397f1f547688874dc4f0"
   end
 
   def install
@@ -32,6 +32,6 @@ class Contig < Formula
   end
 
   test do
-    assert_match "0.18.0", shell_output("#{bin}/contig version")
+    assert_match "0.19.0", shell_output("#{bin}/contig version")
   end
 end
